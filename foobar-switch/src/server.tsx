919a53047bb1a8643b621c1430a6a28b2ddde03d
Jack <jack@jackson.lan>
2020-07-22 16:22:07 +0200

    works but not perfect

--- foobar-switch/src/server.tsx
+++ foobar-switch/src/server.tsx
@@ -1,16 +1,65 @@
 import '../../lib/src/httpswitch'
-import HttpSwitchServer, { IHttpCallback } from '../../lib/src/httpswitch';
+import HttpSwitchServer, { IHttpCallback, IQueryOptions } from '../../lib/src/httpswitch';
+import * as express from 'express';
+import logService from '../../lib/src/services/logService';
+import * as exec from 'child_process';
 
-
+const LOG = logService.getLog("FoobarHttpSwitchServer");
 class FoobarHttpSwitchServer extends HttpSwitchServer {
 
     private readonly setVolumeCallback: IHttpCallback = {
+        callback(options: IQueryOptions, request: express.Request) {
+            LOG.debug(`request called for ${JSON.stringify(options)} with query params=${JSON.stringify(request.query)}`);
+            const params = request.query;
+            if (params["param1"]) {
+                const vol = parseInt(params["param1"].toString());
+                if (vol >= 0 && vol <= 100) {
 
+                }
+                else {
+                    LOG.warn(`invalid volume ${vol} from query ${JSON.stringify(params)}`)
+                }
+            }
+            else {
+                LOG.warn(`no param1 parameter found in query ${JSON.stringify(params)}`)
+            }
+        }
     }
     public constructor() {
-        super({ port: 8889, remoteUrl: "http://10.20.0.180:8888" });
+        super({ port: 8888, remoteUrl: "http://10.20.0.180:8889" });
+        const self = this;
+        this.registerCallback({ url: "cmd=Volume" }, {
+            callback(options: IQueryOptions, request: express.Request) {
+                LOG.debug(`request called for ${JSON.stringify(options)} with query params=${JSON.stringify(request.query)}`);
+                const params = request.query;
+                if (params["param1"]) {
+                    const vol = parseInt(params["param1"].toString());
+                    if (vol >= 0 && vol <= 100) {
+                        self.setSystemVolume(vol);
+                    }
+                    else {
+                        LOG.warn(`invalid volume ${vol} from query ${JSON.stringify(params)}`)
+                    }
+                }
+                else {
+                    LOG.warn(`no param1 parameter found in query ${JSON.stringify(params)}`)
+                }
+            }
+        });
+    }
+    private setSystemVolume(volume: number) {
+        this.exec(`amixer -D pulse sset Master ${volume}%`);
+    }
+    public exec(args: string): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
 
-        this.registerCallback({}, this.setVolumeCallback);
+            const script = exec.exec(`${args}`, (error, stdout, stderr) => {
+                LOG.debug(stdout);
+                resolve();
+            });
+
+        });
+
     }
 }
 
--- lib/src/httpswitch.ts
+++ lib/src/httpswitch.ts
@@ -3,11 +3,13 @@ import * as express from 'express';
 import * as http from 'http';
 import logService from './services/logService';
 import fetch from 'node-fetch';
+import { resolveTxt } from "dns";
 
 export interface IHttpCallback {
-
+    callback(options: IQueryOptions, request: express.Request): void;
 }
 export interface IQueryOptions {
+    url?: string;
     uri?: string;
     queryParams?: string[];
 }
@@ -23,6 +25,9 @@ class HttpSwitchServer {
         this.callbacks = [];
         this.app = express();
         this.app.get("/*", (req, resp) => { this.onGetRequest(req, resp); });
+        this.app.post("/*", (req, resp) => { this.onPostRequest(req, resp); });
+        this.app.put("/*", (req, resp) => { this.onPutRequest(req, resp); });
+        this.app.delete("/*", (req, resp) => { this.onDeleteRequest(req, resp); });
         this.server = http.createServer(this.app);
     }
 
@@ -30,49 +35,111 @@ class HttpSwitchServer {
 
         this.callbacks.forEach((callback) => {
             if (this.matches(callback.options, request)) {
-
+                callback.callback.callback(callback.options, request);
             }
         });
         this.forwardGetRequest(request, response);
+
+    }
+    private onPostRequest(request: express.Request, response: express.Response) {
+
+        this.forwardRequest(request, response);
+
+    }
+    private onPutRequest(request: express.Request, response: express.Response) {
+
+        this.forwardRequest(request, response);
+
+    }
+    private onDeleteRequest(request: express.Request, response: express.Response) {
+
+        this.forwardRequest(request, response);
+
+    }
+    private static kebabToCamelCase(input: string): string {
+        let arr = input.split('-');
+        let capital = arr.map((item, index) => index ? item.charAt(0).toUpperCase() + item.slice(1).toLowerCase() : item);
+        return capital.join("");
     }
     private convertHeaders(headers: http.IncomingHttpHeaders): { [key: string]: string } {
         const data: { [key: string]: string } = {};
         Object.keys(headers).forEach((key) => {
-
+            const ckey = HttpSwitchServer.kebabToCamelCase(key);
+            const value = headers[key];
+            if (value instanceof Array) {
+                data[ckey] = (value as string[]).join(";")
+            }
+            else if (value != null) {
+                data[ckey] = value;
+            }
         });
-        if (headers.accept) {
-            data["Accept"] = headers.accept;
-        }
-        if (headers["content-type"]) {
-            data["Content-Type"] = headers["content-type"];
-        }
-        if (headers.authorization) {
-            data["Authorization"] = headers.authorization;
-        }
         return data;
-        //return { "Accept": headers.accept || "", "Content-Type":headers["content-type"] || , };
     }
     private async forwardGetRequest(request: express.Request, response: express.Response) {
-        const url2 = "";
+        const url2 = this.config.remoteUrl + request.url;
+        LOG.debug("forwarding GET request")
+        LOG.debug(`url=${request.url}`)
+        LOG.debug(`query=${JSON.stringify(request.query)}`)
+        LOG.debug(`params=${JSON.stringify(request.params)}`)
+        LOG.debug(`forward to ${url2}`);
         const result = await fetch(url2, { method: 'GET', headers: this.convertHeaders(request.headers) });
-        response.sendStatus(result.status);
-        result.headers.forEach((header) => {
-            const idx = header.indexOf(":");
+        const resheaders: { [key: string]: string } = {};
+        for (var pair of result.headers.entries()) {
+            LOG.debug(`response header=${pair[0]} : ${pair[1]}`)
+            if (pair[0] == "connection") {
+                continue;
+            }
+            resheaders[pair[0]] = pair[1].replace(/;\s*$/, "");
 
-            response.setHeader(header.substring(0, idx), header.substring(idx + 1))
-        })
-        response.send(result.body)
+        }
+        //resheaders["content-type"] = "text/html; charset=UTF-8"
+        response.status(result.status);
+        response.header(resheaders);
+        response.send(result.body);
+
+    }
+    private async forwardRequest(request: express.Request, response: express.Response) {
+        const url2 = this.config.remoteUrl + request.url;
+        LOG.debug(`forwarding ${request.method} request`)
+        LOG.debug(`url=${request.url}`)
+        LOG.debug(`query=${JSON.stringify(request.query)}`)
+        LOG.debug(`params=${JSON.stringify(request.params)}`)
+        LOG.debug(`forward to ${url2}`);
+        const result = await fetch(url2, { method: request.method, body: request.body, headers: this.convertHeaders(request.headers) });
+        const resheaders: { [key: string]: string } = {};
+        for (var pair of result.headers.entries()) {
+            LOG.debug(`response header=${pair[0]} : ${pair[1]}`)
+            if (pair[0] == "connection") {
+                continue;
+            }
+            resheaders[pair[0]] = pair[1].replace(/;\s*$/, "");
+
+        }
+        response.status(result.status);
+        response.header(resheaders);
+        response.send(result.body);
+
     }
     private matches(options: IQueryOptions, request: express.Request): boolean {
+        const idx = request.url.indexOf("?");
+        const uri = idx < 0 ? request.url : request.url.substring(0, idx);
+        const query = request.query;
+        if (options.url) {
+            LOG.debug(`test match for ${request.url} and ${options.url}`)
+            if (request.url.indexOf(options.url) > -1) {
+                LOG.debug("match")
+                return true;
+            }
+        }
         return false;
     }
     public registerCallback(options: IQueryOptions, callback: IHttpCallback) {
-
+        this.callbacks.push({ options: options, callback: callback });
     }
 
     public start(): void {
         this.server.listen(this.config.port, () => {
-            LOG.debug('Server started listening on port ' + this.config.port + '.');
+            LOG.info('Server started listening on port ' + this.config.port + '.');
         });
     }
     public stop(): void {
